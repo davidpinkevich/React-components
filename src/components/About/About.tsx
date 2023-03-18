d2dcd690b9fcb1b43dafcac2a3ae4cb8855dc1b6
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-18 23:32:17 +0300

    feat: create about page

---
+++ src/components/About/About.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+export class About extends Component {
+  render() {
+    const styleHome = '__container';
+    return <main className={styleHome}>About</main>;
+  }
+}
+
+export default About;
