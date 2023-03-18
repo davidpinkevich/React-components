1b3ce53675d1e9de7c6ea6c8a6c18997faff3948
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-18 23:30:54 +0300

    feat: add 404 page

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import styles from './NotFound.module.scss';
+
+export class NotFound extends Component {
+  render() {
+    return <div className={styles.notFound}>Not Found</div>;
+  }
+}
+
+export default NotFound;
