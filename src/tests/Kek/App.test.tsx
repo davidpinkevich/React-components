42c7cd31fdc6204a64996c915ad6b2a12e6316f3
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-19 22:57:16 +0300

    feat: add test for app

---
+++ src/tests/Kek/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from '../../components/App/App';
+
+describe('App component', () => {
+  it('renders home page by default', () => {
+    render(<App />);
+    const homeElement = screen.getByText('Home');
+    expect(homeElement).toBeInTheDocument();
+  });
+});
