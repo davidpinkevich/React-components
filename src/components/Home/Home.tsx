122954f2abc357bfa140fbbb99039d8cc0d1dbb8
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-19 13:28:27 +0300

    feat: create search

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,19 @@
 import { Component } from 'react';
 import ListItems from './ListItems/ListItems';
+import SearchInput from './SearchInput/SearchInput';
 
 export class Home extends Component {
+  state = {
+    search: '',
+  };
+  udpateSearch = (search: string): void => {
+    this.setState({ search });
+  };
   render() {
     const styleHome = '__container';
     return (
       <main className={styleHome}>
+        <SearchInput udpateSearch={this.udpateSearch} />
         <ListItems />
       </main>
     );
