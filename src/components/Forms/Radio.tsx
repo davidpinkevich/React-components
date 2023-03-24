b0ad0f64b2242eef79925859bdf0179ef6ceee35
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-24 20:21:29 +0300

    feat: add input radio for form

---
+++ src/components/Forms/Radio.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import { TInputsRadio } from '../../types/types';
+
+export class Radio extends Component<TInputsRadio> {
+  constructor(props: TInputsRadio) {
+    super(props);
+  }
+  render() {
+    const arrayRadio = ['one', 'two', 'three'];
+    return (
+      <>
+        {arrayRadio.map((elem, index) => {
+          return (
+            <label key={index}>
+              <input type="radio" ref={this.props.values[index]} name="question" value={elem} />
+              {elem}
+            </label>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default Radio;
