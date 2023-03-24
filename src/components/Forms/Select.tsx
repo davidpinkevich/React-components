c9790263b7500171e31af7b8a79636a241a13782
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-24 20:21:46 +0300

    feat: add select for form

---
+++ src/components/Forms/Select.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import { TInputsSelect } from '../../types/types';
+
+class Select extends Component<TInputsSelect> {
+  constructor(props: TInputsSelect) {
+    super(props);
+  }
+  render() {
+    const countries = ['Belarus', 'Russia', 'Kazakhstan', 'Georgia'];
+    return (
+      <select ref={this.props.values}>
+        {countries.map((elem, index) => (
+          <option key={index} value={elem}>
+            {elem}
+          </option>
+        ))}
+      </select>
+    );
+  }
+}
+
+export default Select;
