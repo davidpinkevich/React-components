3e0294f7cd536015e6620fc828c1e113fe2d1959
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-24 20:21:54 +0300

    feat: add forms

---
+++ src/components/Forms/Forms.tsx
@@ -0,0 +1,46 @@
+import React, { Component, createRef } from 'react';
+import { TPropsForm, TStateForm } from '../../types/types';
+import Radio from './Radio';
+import Select from './Select';
+import Checkbox from './Checkbox';
+import FileInput from './FileInput';
+
+class Forms extends Component<TPropsForm, TStateForm> {
+  inputName = createRef<HTMLInputElement>();
+  inputDate = createRef<HTMLInputElement>();
+  inputSelect = createRef<HTMLSelectElement>();
+  inputsRadio = new Array(3).fill('').map(() => createRef<HTMLInputElement>());
+  inputsCheckbox = new Array(4).fill('').map(() => createRef<HTMLInputElement>());
+  inputFile = createRef<HTMLInputElement>();
+  checked = React.createRef();
+  constructor(props: TPropsForm) {
+    super(props);
+  }
+  render() {
+    return (
+      <form
+        noValidate
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+          this.props.onSubmit(event, {
+            name: this.inputName.current?.value ? this.inputName.current?.value : '',
+            date: this.inputDate.current?.value ? this.inputDate.current?.value : '',
+            select: this.inputSelect.current?.value ? this.inputSelect.current?.value : '',
+            radio: this.inputsRadio.map((elem) => elem.current?.checked),
+            checkbox: this.inputsCheckbox.map((elem) => elem.current?.checked),
+            file: this.inputFile.current?.files?.length ? this.inputFile.current.files[0] : '',
+          })
+        }
+      >
+        <input type="text" ref={this.inputName} />
+        <input type="date" ref={this.inputDate} />
+        <Select values={this.inputSelect} />
+        <Radio values={[...this.inputsRadio]} />
+        <Checkbox values={[...this.inputsCheckbox]} />
+        <FileInput values={this.inputFile} />
+        <input type="submit" value="Отправить" />
+      </form>
+    );
+  }
+}
+
+export default Forms;
--- src/components/Forms/PageForms.tsx
+++ src/components/Forms/PageForms.tsx
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
+import { TPageForms, TItemForm } from '../../types/types';
+import Forms from './Forms';
 
-class PageForms extends Component {
+class PageForms extends Component<Record<string, void>, TPageForms> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      value: [],
+    };
+  }
+
+  validateForm = (obj: TItemForm) => {
+    const objForValidate = { ...obj };
+    console.log('objForValidate: ', objForValidate);
+    const regexpName = /[A-Z][a-z]{2,}\s[A-Z][a-z]{2,}/;
+    const regexpDate = /\d{4}\-\d{2}\-\d{2}/;
+    const checkYear = Number(objForValidate.date.match(/\d{4}/));
+    return {
+      checkName: regexpName.test(objForValidate.name) ? true : false,
+      checkDate:
+        regexpDate.test(objForValidate.date) && checkYear >= 1930 && checkYear <= 2005
+          ? true
+          : false,
+      checkRadio: objForValidate.radio,
+      checkSelect: objForValidate.select,
+      checkCheckbox: objForValidate.checkbox,
+      checkFile: objForValidate.file,
+    };
+  };
+
+  changeSub = (event: React.FormEvent<HTMLFormElement>, obg: TItemForm) => {
+    event.preventDefault();
+    const testStr = this.validateForm(obg);
+    console.log(testStr);
+    const args = [...this.state.value, obg];
+    this.setState({ value: args });
+  };
+
   render() {
-    return <div>PageForms</div>;
+    return (
+      <>
+        <Forms onSubmit={this.changeSub} />
+      </>
+    );
   }
 }
 
