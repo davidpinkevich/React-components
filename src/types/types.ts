70483856e83736d46141287107fb060663c3dc3c
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-24 20:20:39 +0300

    feat: add types for inputs

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,6 +14,43 @@ export type TPropsInput = {
   udpateSearch: (arg0: string) => void;
 };
 
+export type TPropsForm = {
+  onSubmit: (event: React.FormEvent<HTMLFormElement>, arg0: TItemForm) => void;
+};
+
 export type TStateInput = {
   search: string;
+};
+
+export type TStateForm = {
+  value: string;
+};
+
+export type TItemForm = {
+  name: string;
+  date: string;
+  select: string;
+  radio: Array<boolean | undefined>;
+  checkbox: Array<boolean | undefined>;
+  file: string | File;
+};
+
+export type TPageForms = {
+  value: Array<TItemForm>;
+};
+
+export type TInputsRadio = {
+  values: Array<React.RefObject<HTMLInputElement>>;
+};
+
+export type TInputsSelect = {
+  values: React.RefObject<HTMLSelectElement>;
+};
+
+export type TInputsCheckbox = {
+  values: Array<React.RefObject<HTMLInputElement>>;
+};
+
+export type TInputsFile = {
+  values: React.RefObject<HTMLInputElement>;
 };
