9d0ec99e1c9e851a47055a029523ab562c064858
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-24 20:21:02 +0300

    feat: add checkbox for form

---
+++ src/components/Forms/Checkbox.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import { TInputsCheckbox } from '../../types/types';
+
+export class Checkbox extends Component<TInputsCheckbox> {
+  constructor(props: TInputsCheckbox) {
+    super(props);
+  }
+  render() {
+    const hobbies = ['Motorcycling', 'Swimming', 'Hunting', 'Alcohol'];
+    return (
+      <>
+        {this.props.values.map((elem, index) => {
+          return (
+            <label key={index}>
+              {hobbies[index]}
+              <input type="checkbox" ref={this.props.values[index]} name={hobbies[index]} />
+            </label>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default Checkbox;
