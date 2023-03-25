9e81da535188018f10eed200a1298924c803a318
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-25 17:14:53 +0300

    feat: add validate and error for checkbox

--- src/components/PageForms/Checkbox/Checkbox.tsx
+++ src/components/PageForms/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { TInputsCheckbox } from '../../../types/types';
+import styles from './Checkbox.module.scss';
 
 export class Checkbox extends Component<TInputsCheckbox> {
   constructor(props: TInputsCheckbox) {
@@ -8,16 +9,24 @@ export class Checkbox extends Component<TInputsCheckbox> {
   render() {
     const hobbies = ['Motorcycling', 'Swimming', 'Hunting', 'Alcohol'];
     return (
-      <>
-        {this.props.values.map((elem, index) => {
-          return (
-            <label key={index}>
-              {hobbies[index]}
-              <input type="checkbox" ref={this.props.values[index]} name={hobbies[index]} />
-            </label>
-          );
-        })}
-      </>
+      <div className={styles.radioWrapper}>
+        <div className={styles.checkbox}>
+          <p>Choose your passion:</p>
+          <div className={styles.checkboxInput}>
+            <div className={styles.labelsWrapper}>
+              {this.props.values.map((elem, index) => {
+                return (
+                  <label key={index}>
+                    {hobbies[index]}
+                    <input type="checkbox" ref={this.props.values[index]} name={hobbies[index]} />
+                  </label>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        {!this.props.valid && <p className={styles.inputError}>Model must be selected</p>}
+      </div>
     );
   }
 }
