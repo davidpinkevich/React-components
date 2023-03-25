77a11d95f30d415d0370d8be344fccf0eb48711e
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-25 17:16:04 +0300

    feat: add validate and error for forms

--- src/components/PageForms/Forms/Forms.tsx
+++ src/components/PageForms/Forms/Forms.tsx
@@ -4,6 +4,7 @@ import Radio from '../Radio/Radio';
 import Select from '../Select/Select';
 import Checkbox from '../Checkbox/Checkbox';
 import FileInput from '../FileInput/FileInput';
+import styles from './Forms.module.scss';
 
 class Forms extends Component<TPropsForm, TStateForm> {
   inputName = createRef<HTMLInputElement>();
@@ -19,6 +20,7 @@ class Forms extends Component<TPropsForm, TStateForm> {
   render() {
     return (
       <form
+        className={styles.mainForm}
         noValidate
         onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
           this.props.onSubmit(event, {
@@ -34,13 +36,35 @@ class Forms extends Component<TPropsForm, TStateForm> {
           })
         }
       >
-        <input type="text" ref={this.inputName} />
-        <input type="date" ref={this.inputDate} />
-        <Select values={this.inputSelect} />
-        <Radio values={[...this.inputsRadio]} />
-        <Checkbox values={[...this.inputsCheckbox]} />
-        <FileInput values={this.inputFile} />
-        <input type="submit" value="Отправить" />
+        <h1>Who are you traveler?</h1>
+        <div className={styles.inputSelect}>
+          <p className={styles.question}>Enter the first and last name with a capital letter:</p>
+          <input type="text" ref={this.inputName} />
+          {!this.props.valid.checkName && (
+            <p className={styles.inputError}>
+              You must enter the first and last name with a capital letter (minimum 3 letters per
+              word)
+            </p>
+          )}
+          <p className={styles.question}>Enter your date of birth:</p>
+          <input className={styles.inputDate} type="date" ref={this.inputDate} />
+          {!this.props.valid.checkDate && (
+            <p className={styles.inputError}>
+              A user must be at least 18 years of age to create a card (2005)
+            </p>
+          )}
+          <p className={styles.question}>Where are you from:</p>
+          <Select values={this.inputSelect} />
+        </div>
+        <Radio valid={this.props.valid.checkRadio} values={[...this.inputsRadio]} />
+        <Checkbox valid={this.props.valid.checkCheckbox} values={[...this.inputsCheckbox]} />
+        <FileInput
+          values={this.inputFile}
+          changeNames={this.props.changeName}
+          fileName={this.props.fileName}
+          valid={this.props.valid.checkFileName}
+        />
+        <input className="btnSubmit" type="submit" value="Create Card" />
       </form>
     );
   }
