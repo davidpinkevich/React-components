fa30a5f3600dafe880c5df1956e665f7838526d6
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-25 17:14:34 +0300

    feat: add validate and error for radio

--- src/components/PageForms/Radio/Radio.tsx
+++ src/components/PageForms/Radio/Radio.tsx
@@ -1,23 +1,28 @@
 import { Component } from 'react';
 import { TInputsRadio } from '../../../types/types';
+import styles from './Radio.module.scss';
 
 export class Radio extends Component<TInputsRadio> {
   constructor(props: TInputsRadio) {
     super(props);
   }
   render() {
-    const arrayRadio = ['Mercedes', 'Toyota', 'Nissan'];
+    const arrayRadio = ['Mercedes', 'Toyota', 'Lada'];
     return (
-      <>
-        {arrayRadio.map((elem, index) => {
-          return (
-            <label key={index}>
-              <input type="radio" ref={this.props.values[index]} name="question" value={elem} />
-              {elem}
-            </label>
-          );
-        })}
-      </>
+      <div className={styles.radioWrapper}>
+        <div className={styles.radioBody}>
+          <p>Make the Right Choice: </p>
+          {arrayRadio.map((elem, index) => {
+            return (
+              <label key={index}>
+                <input type="radio" ref={this.props.values[index]} name="question" value={elem} />
+                {elem}
+              </label>
+            );
+          })}
+        </div>
+        {!this.props.valid && <p className={styles.inputError}>Model must be selected</p>}
+      </div>
     );
   }
 }
