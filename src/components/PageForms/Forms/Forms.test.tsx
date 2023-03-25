770b960b6bd1ba7a4ed8e9d4126ece0aff8797f5
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-25 21:51:49 +0300

    feat: add test for forms

---
+++ src/components/PageForms/Forms/Forms.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import Forms from './Forms';
+
+describe('Forms component', () => {
+  it('render forms', () => {
+    const valid = {
+      checkName: false,
+      checkDate: false,
+      checkRadio: false,
+      checkCheckbox: false,
+      checkFileName: false,
+    };
+    const changeName = () => {
+      console.log('mock');
+    };
+    const changeSub = () => {
+      console.log('mock');
+    };
+    render(<Forms valid={valid} fileName={false} onSubmit={changeSub} changeName={changeName} />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Who are you traveler?');
+    // const fileElement = screen.getByText('Who are you traveler?');
+    // expect(fileElement).toBeInTheDocument();
+  });
+});
