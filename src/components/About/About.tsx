d19c3be3fd9951b3b6d97eac05d17cc8c7d34d00
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-26 22:54:34 +0300

    feat: rewrite About to functional component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,6 @@
-import { Component } from 'react';
-
-export class About extends Component {
-  render() {
-    const styleHome = '__container';
-    return <main className={styleHome}>About</main>;
-  }
-}
+const About = () => {
+  const styleHome = '__container';
+  return <main className={styleHome}>About</main>;
+};
 
 export default About;
