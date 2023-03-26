d2dd756034a334df5fa66913b193211a7881db88
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-26 19:28:39 +0300

    feat: rewrite Item to functional component

--- src/components/Home/Item/Item.tsx
+++ src/components/Home/Item/Item.tsx
@@ -1,25 +1,20 @@
-import { Component } from 'react';
 import { Ibook } from '../../../types/types';
 import styles from './Item.module.scss';
 
-class Item extends Component<Ibook> {
-  constructor(props: Ibook) {
-    super(props);
-  }
-  render() {
-    const { title, author, genre, year, image } = this.props;
-    return (
-      <li className={styles.item}>
-        <div className={styles.bookImgWrapper}>
-          <img src={image} />
-        </div>
-        <h2>{title}</h2>
-        <p>Author: {author}</p>
-        <p>Genre: {genre}</p>
-        <p>Year: {year}</p>
-      </li>
-    );
-  }
-}
+const Item = (props: Ibook) => {
+  const { title, author, genre, year, image } = props;
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.bookImgWrapper}>
+        <img src={image} />
+      </div>
+      <h2>{title}</h2>
+      <p>Author: {author}</p>
+      <p>Genre: {genre}</p>
+      <p>Year: {year}</p>
+    </li>
+  );
+};
 
 export default Item;
