b4e28b5230f47a1ac2234df411a46f6d6fbcdaee
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-26 19:40:34 +0300

    feat: rewrite Menu to functional component

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -1,57 +1,45 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Menu.module.scss';
-type CounterState = {
-  location: string;
-};
 
-export class Menu extends Component<Record<string, never>, CounterState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = {
-      location: window.location.pathname,
-    };
-  }
-
-  changeLocal = () => {
-    this.setState(() => ({
-      location: window.location.pathname,
-    }));
+const Menu = () => {
+  const [location, setLocation] = useState(window.location.pathname);
+  const changeLocal = () => {
+    setLocation(window.location.pathname);
   };
-  render() {
-    const path = this.state.location;
-    return (
-      <div className={styles.headerNavigation}>
-        <div className={styles.currentPage}>
-          <p>Current page:</p>
-          {path === '/' ? 'Home' : path === '/about' ? 'About Us' : 'Forms'}
-        </div>
-        <nav className={styles.navigation}>
-          <NavLink
-            onClick={this.changeLocal}
-            className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
-            to="."
-          >
-            Home
-          </NavLink>
-          <NavLink
-            onClick={this.changeLocal}
-            className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
-            to="forms"
-          >
-            Forms
-          </NavLink>
-          <NavLink
-            onClick={this.changeLocal}
-            className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
-            to="about"
-          >
-            About Us
-          </NavLink>
-        </nav>
+
+  const path = location;
+  return (
+    <div className={styles.headerNavigation}>
+      <div className={styles.currentPage}>
+        <p>Current page:</p>
+        {path === '/' ? 'Home' : path === '/about' ? 'About Us' : 'Forms'}
       </div>
-    );
-  }
-}
+      <nav className={styles.navigation}>
+        <NavLink
+          onClick={changeLocal}
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
+          to="."
+        >
+          Home
+        </NavLink>
+        <NavLink
+          onClick={changeLocal}
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
+          to="forms"
+        >
+          Forms
+        </NavLink>
+        <NavLink
+          onClick={changeLocal}
+          className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
+          to="about"
+        >
+          About Us
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
 
 export default Menu;
