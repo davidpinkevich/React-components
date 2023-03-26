e84c23cc2376a04559ae6f826630e669496bd848
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-26 22:57:02 +0300

    feat: rewrite App to functional component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Component } from 'react';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import About from '../About/About';
@@ -7,23 +6,21 @@ import NotFound from '../NotFound/NotFound';
 import PageForms from '../PageForms/PageForms';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Header />}>
-              <Route index element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="forms" element={<PageForms />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="forms" element={<PageForms />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
