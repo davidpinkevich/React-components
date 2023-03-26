967a2ed02f4e86efc5839640a2af6d8fb0897142
davidpinkevich <davidpinkevich95@gmail.com>
2023-03-26 19:42:19 +0300

    feat: rewrite NotFound to functional component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,10 +1,5 @@
-import { Component } from 'react';
 import styles from './NotFound.module.scss';
 
-export class NotFound extends Component {
-  render() {
-    return <main className={styles.notFound}>Not Found</main>;
-  }
-}
+const NotFound = () => <main className={styles.notFound}>Not Found</main>;
 
 export default NotFound;
