ce51beace8f9dabfc2a3228f917f526252124ddc
davidpinkevich <davidpinkevich95@gmail.com>
2023-04-01 00:39:49 +0300

    feat: rewrite Home to functional component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,24 +1,5 @@
-// import { Component } from 'react';
 import ListItems from './ListItems/ListItems';
 import SearchInput from './SearchInput/SearchInput';
-
-// export class Home extends Component {
-//   state = {
-//     search: '',
-//   };
-//   udpateSearch = (search: string): void => {
-//     this.setState({ search });
-//   };
-//   render() {
-//     const styleHome = '__container';
-//     return (
-//       <main className={styleHome}>
-//         <SearchInput udpateSearch={this.udpateSearch} />
-//         <ListItems />
-//       </main>
-//     );
-//   }
-// }
 
 const Home = () => {
   const styleHome = '__container';
