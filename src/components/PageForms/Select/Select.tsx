9b9d5dac32f7fcf3f73c3367ed6b2969abb71587
davidpinkevich <davidpinkevich95@gmail.com>
2023-04-01 00:40:02 +0300

    feat: rewrite Select to functional component

--- src/components/PageForms/Select/Select.tsx
+++ src/components/PageForms/Select/Select.tsx
@@ -1,22 +1,16 @@
-import { Component } from 'react';
 import { TInputsSelect } from '../../../types/types';
 
-class Select extends Component<TInputsSelect> {
-  constructor(props: TInputsSelect) {
-    super(props);
-  }
-  render() {
-    const countries = ['Belarus', 'Russia', 'Kazakhstan', 'Georgia'];
-    return (
-      <select ref={this.props.values}>
-        {countries.map((elem, index) => (
-          <option key={index} value={elem}>
-            {elem}
-          </option>
-        ))}
-      </select>
-    );
-  }
-}
+const Select = (props: TInputsSelect) => {
+  const countries = ['Belarus', 'Russia', 'Kazakhstan', 'Georgia'];
+  return (
+    <select {...props.register('select')}>
+      {countries.map((elem, index) => (
+        <option key={index} value={elem}>
+          {elem}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 export default Select;
