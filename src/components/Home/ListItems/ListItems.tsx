dd473e9b16e7e42200bce71c842c1a475cf288fd
davidpinkevich <davidpinkevich95@gmail.com>
2023-04-15 20:28:16 +0300

    feat: create renderWithProviders for test redux

--- src/components/Home/ListItems/ListItems.tsx
+++ src/components/Home/ListItems/ListItems.tsx
@@ -6,8 +6,6 @@ const ListItems = (props: TListItems) => {
   return (
     <ul className={styles.listItems}>
       {props.results.map((elem) => {
-        // return <Item {...elem} getId={props.getId} changePopup={props.changePopup} key={elem.id} />;
-        // return <Item {...elem} getId={props.getId} key={elem.id} />;
         return <Item {...elem} key={elem.id} />;
       })}
     </ul>
