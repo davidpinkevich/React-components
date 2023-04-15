4205b048ac58ed5f34c6ec010f27660a6d62dcbc
davidpinkevich <davidpinkevich95@gmail.com>
2023-04-15 20:30:53 +0300

    feat: change test App for redux

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithProviders from '../../tests/renderWithProviders';
 import App from './App';
 
 describe('App component', () => {
   it('renders home page by default', () => {
-    render(<App />);
+    renderWithProviders(<App />);
     const homeElement = screen.getByText('Current page:');
     expect(homeElement).toBeInTheDocument();
   });
