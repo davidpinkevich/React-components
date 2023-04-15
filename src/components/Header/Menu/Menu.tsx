a6fb7af9165f4be21f5a3c2154a6b79a1cdabd93
davidpinkevich <davidpinkevich95@gmail.com>
2023-04-15 16:33:55 +0300

    feat: add location for menu

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -1,37 +1,29 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from './Menu.module.scss';
 
 const Menu = () => {
-  const [location, setLocation] = useState(window.location.pathname);
-
-  const changeLocal = () => {
-    setTimeout(() => setLocation(window.location.pathname), 0);
-  };
+  const location = useLocation();
 
   return (
     <div className={styles.headerNavigation}>
       <div className={styles.currentPage}>
         <p>Current page:</p>
-        {location === '/' ? 'Home' : location === '/about' ? 'About Us' : 'Forms'}
+        {location.pathname === '/' ? 'Home' : location.pathname === '/about' ? 'About Us' : 'Forms'}
       </div>
       <nav className={styles.navigation}>
         <NavLink
-          onClick={changeLocal}
           className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
           to="."
         >
           Home
         </NavLink>
         <NavLink
-          onClick={changeLocal}
           className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
           to="forms"
         >
           Forms
         </NavLink>
         <NavLink
-          onClick={changeLocal}
           className={({ isActive }) => (isActive ? styles.activeLink : styles.notActiveLink)}
           to="about"
         >
