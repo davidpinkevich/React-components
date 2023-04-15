45555834df868032b4749f5b52af4c622f6e1d41
davidpinkevich <davidpinkevich95@gmail.com>
2023-04-15 16:28:54 +0300

    feat: add logics redux for Page forms

--- src/components/PageForms/PageForms.tsx
+++ src/components/PageForms/PageForms.tsx
@@ -1,37 +1,17 @@
-import { useState } from 'react';
-import { TItemForm } from '../../types/types';
+import { useSelector } from 'react-redux';
+import { TStore } from '../../types/types';
 import Forms from './Forms/Forms';
 import ItemsForm from './ItemsForm/ItemsForm';
 import styles from './PageForms.module.scss';
 
 const PageForms = () => {
-  const [values, setValues] = useState<Array<TItemForm>>([]);
-  const [popup, setPopup] = useState(false);
-  const [fileName, setfileName] = useState(false);
-
-  const changeSub = (obg: TItemForm) => {
-    setValues([...values, obg]);
-  };
-
-  const changeName = (value: boolean) => {
-    setfileName(value);
-  };
-
-  const changePopup = (value: boolean) => {
-    setPopup(value);
-  };
-
+  const { items, popup } = useSelector((state: TStore) => state.form);
   return (
     <>
       {popup && <p className={styles.cardSuccessfully}>Card created successfully!</p>}
-      <Forms
-        changeName={changeName}
-        changeSub={changeSub}
-        fileName={fileName}
-        changePopup={changePopup}
-      />
-      {values.length ? <p className={styles.list}>List of cards:</p> : ''}
-      <ItemsForm value={values} />
+      <Forms />
+      {items.length ? <p className={styles.list}>List of cards:</p> : ''}
+      <ItemsForm value={items} />
     </>
   );
 };
